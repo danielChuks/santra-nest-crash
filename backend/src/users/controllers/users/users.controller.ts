2550bc0f31216e2f37fe178bad4c26be6a75b609
Daniel chuks <danieluchechukwuchuks1@gmail.com>
2023-12-05 12:31:24 +0100

    setting up

---
+++ backend/src/users/controllers/users/users.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from '../../dtos/CreateUser.dto';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers(@Query('sortBy') sortBy: string) {
+    console.log(sortBy);
+    return { hello: 'users', password: 'password' };
+  }
+
+  @Post('create')
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return {};
+  }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    console.log(id);
+    return { id };
+  }
+}
